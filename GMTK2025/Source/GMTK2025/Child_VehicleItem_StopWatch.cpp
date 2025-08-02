a82796d63d098f3681aee0b8ab6c05ccd0882085
Thegamingchance <42433225+Thegamingchance@users.noreply.github.com>
2025-08-01 19:21:46 -0600

    stop watch now removes itself after a certain period and returns the time to normal

--- GMTK2025/Source/GMTK2025/Child_VehicleItem_StopWatch.cpp
+++ GMTK2025/Source/GMTK2025/Child_VehicleItem_StopWatch.cpp
@@ -12,11 +12,22 @@ void UChild_VehicleItem_StopWatch::BeginPlay()
 
 void UChild_VehicleItem_StopWatch::UseItem()
 {
-	UGameplayStatics::SetGlobalTimeDilation(GetWorld(), 0.5f); // Slow down time to half speed
-
+	// Check if the item has not been used yet
+	if (bHasNotBeenUsed == true)
+	{
+		bHasNotBeenUsed = false;
+		UGameplayStatics::SetGlobalTimeDilation(GetWorld(), 0.5f); // Slow down time to half speed
+		GetWorld()->GetTimerManager().SetTimer(MyTimerHandle, this, &UChild_VehicleItem_StopWatch::CountUpTimer, 1.0f, true);
+	}
+	else
+	{
+		
+	}
 }
 
 void UChild_VehicleItem_StopWatch::RemoveItem()
 {
+	UGameplayStatics::SetGlobalTimeDilation(GetWorld(), 1.0f); // Reset time dilation to normal speed
 	DestroyComponent();
 }
+
