e9d736fa9d0b1c33e8ab3e99a788d5bc3f6249bd
Thegamingchance <42433225+Thegamingchance@users.noreply.github.com>
2025-08-01 23:54:45 -0600

    coins error

---
+++ GMTK2025/Source/GMTK2025/Child_PickupClass_CoinCollect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickup_Base.h"
+#include "Child_PickupClass_CoinCollect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GMTK2025_API AChild_PickupClass_CoinCollect : public APickup_Base
+{
+	GENERATED_BODY()
+	
+};
