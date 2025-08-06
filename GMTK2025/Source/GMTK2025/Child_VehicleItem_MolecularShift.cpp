e9648e0f186e664560abc7f91112379d9e76fdf0
cachandlerdev <70250943+cachandlerdev@users.noreply.github.com>
2025-08-06 14:31:52 -0700

    fix: add sfx to pickups (need to rework some pickup code soon though)

--- GMTK2025/Source/GMTK2025/Child_VehicleItem_MolecularShift.cpp
+++ GMTK2025/Source/GMTK2025/Child_VehicleItem_MolecularShift.cpp
@@ -7,6 +7,7 @@
 
 void UChild_VehicleItem_MolecularShift::UseItem()
 {
+	Super::UseItem();
 	TArray<AActor*> FoundActors;
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerGhostActor::StaticClass(), FoundActors);
 
